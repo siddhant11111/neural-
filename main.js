53aaf3c3835bf1860a60416d70fb1533dc4cf9b2
siddhant11111 <90629179+siddhant11111@users.noreply.github.com>
2022-02-23 18:55:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+webcam.attach('#camera');
+camera = document.getElementById("camera");
+
+
+webcam.set({
+    width:350,
+    heigth:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+
+
+
+function take_snapshot()
+{
+    webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:' , ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/[...]',modelLoaded);
+
+function modelLoaded()  {
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = result[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = result[0].confidence.toFixed(3);
+    }
+}
